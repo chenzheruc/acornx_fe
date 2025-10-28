4ef2878d23491722f6d48639b7d438c5fa9dc4cc
Shirley Chen <chenzhe@Shirleys-MacBook-Pro.local>
2025-10-27 19:15:13 -0700

    move tailwindcss to script.js

---
+++ public/script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				primary: "#3db8f5",
+				"background-light": "#f8fbfc",
+				"background-dark": "#101c22",
+			},
+			fontFamily: {
+				display: ["Plus Jakarta Sans", "Noto Sans", "sans-serif"],
+			},
+			borderRadius: {
+				DEFAULT: "0.5rem",
+				lg: "1rem",
+				xl: "1.5rem",
+				full: "9999px",
+			},
+		},
+	},
+};
